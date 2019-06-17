55fcbcd9775f5f6d6fe5aedcbd47a38eb685a01d
xiaojun <xiaojun@itcast.cn>
2019-06-17 20:13:22 +0800

    随便写的

---
+++ untitled/src/com/Asss.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Asss {
+    public static void main(String[] args) {
+        System.out.println("cadkbdabkb");
+        System.out.println("cadkbdabkb");
+        System.out.println("cadkbdabkb");
+        System.out.println("cadkbdabkb");
+    }
+}
